f9c67fdc63a826d92f757bcf185f2885cd8d48c5
jorgeav527 <jorgeav527@gmail.com>
2019-04-16 22:31:19 -0500

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello word
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App;
